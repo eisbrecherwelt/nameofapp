16d7f51121bf80ecdbcad29d68cf75525ff1bbc5
eisbrecherwelt <eisbrecherwelt@gmail.com>
2018-03-09 15:07:56 -0600

    Published for troubleshooting

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,21 +1,30 @@
 require 'rails_helper'
 
 describe Product do
-  context "When the product has comments" do
-  	let(:product) { Product.create!(name: "munchies") }
+  
+  context "when the product has comments" do
+    let(:product) { Product.create!(name: "cheetos") }
+
+    it "should return the product" do
+      expect(@product.name).to eq "cheetos"
+    end
   end
 
-  context "Something about a user" do
+  context "create a user" do
     let(:user) { User.create!(first_name: "John", last_name: "Wick", email: "johnwick041298@gmail.com", encrypted_password: "youkilledmydog")}
+
+    it "should return the user" do
+      expect(@user.first_name).to eq "John"
+    end
   end
 
   before do
-    Product.comments.create!(rating: 1, user: user, body: "Awful munchie!")
-    Product.comments.create!(rating: 3, user: user, body: "Ok munchie!")
-    Product.comments.create!(rating: 5, user: user, body: "Great munchie!")
+    @product.comments.create!(rating: 1, user: user, body: "Awful bike!")
+    @product.comments.create!(rating: 3, user: user, body: "Ok bike!")
+    @product.comments.create!(rating: 5, user: user, body: "Great bike!")
   end
 
   it "returns the average rating of all comments" do
-    expect(Product.average_rating).to eq 3
+    expect(@product.average_rating).to eq 3
   end
 end
